bfbb401cc44d990eefeffe60b1233a082ded994e
Avanthii09 <avanthuram@gmail.com>
2023-03-30 12:13:15 +0530

    added files

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import "./App.css"
+// import Timer from "./components/Timer";
+import Todolist from "./components/Todolist"    //<Todolist /> in return
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import {useState} from "react";
+// import Settings from "./components/Settings";
+// import Settingscontext from "./components/Settingscontext";
+
+function App() {
+
+// const [showSettings, setShowSettings] = useState(false);
+// const [workMinutes, setWorkMinutes] = useState(45);
+// const [breakMinutes, setBreakMinutes] = useState(15);
+
+
+ 
+  // return(
+  //   <main>
+  //     <Settingscontext.Provider value={{
+  //       showSettings,
+  //       setShowSettings,
+  //       workMinutes,
+  //       breakMinutes,
+  //       setWorkMinutes,
+  //       setBreakMinutes,
+  //     }}>
+  //       {showSettings ? <Settings /> : <Timer/>}
+  //     </Settingscontext.Provider>
+    
+
+
+  //   </main>
+  // )
+
+      return(
+        <Todolist />
+      )
+};
+
+export default App;
+
+
+
+
