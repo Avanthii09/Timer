1ee7f4c40997f9837c3238c434d2e4b4833c5e12
Avanthii09 <avanthuram@gmail.com>
2023-04-01 23:22:35 +0530

    added files

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,42 @@
-import "./App.css"
-// import Timer from "./components/Timer";
-import Todolist from "./components/Todolist"    //<Todolist /> in return
+//import "./Todo.css"
+// import "./App.css"
+import "./Slider.css"
+import Timer from "./components/Timer";
+//import Todolist from "./components/Todolist"    //<Todolist /> in return
 import 'bootstrap/dist/css/bootstrap.min.css'
-// import {useState} from "react";
-// import Settings from "./components/Settings";
-// import Settingscontext from "./components/Settingscontext";
+ import {useState} from "react";
+ import Settings from "./components/Settings";
+ import Settingscontext from "./components/Settingscontext";
 
 function App() {
 
-// const [showSettings, setShowSettings] = useState(false);
-// const [workMinutes, setWorkMinutes] = useState(45);
-// const [breakMinutes, setBreakMinutes] = useState(15);
+ const [showSettings, setShowSettings] = useState(false);
+ const [workMinutes, setWorkMinutes] = useState(45);
+ const [breakMinutes, setBreakMinutes] = useState(15);
 
 
  
-  // return(
-  //   <main>
-  //     <Settingscontext.Provider value={{
-  //       showSettings,
-  //       setShowSettings,
-  //       workMinutes,
-  //       breakMinutes,
-  //       setWorkMinutes,
-  //       setBreakMinutes,
-  //     }}>
-  //       {showSettings ? <Settings /> : <Timer/>}
-  //     </Settingscontext.Provider>
+  return(
+    <main>
+      <Settingscontext.Provider value={{
+        showSettings,
+        setShowSettings,
+        workMinutes,
+        breakMinutes,
+        setWorkMinutes,
+        setBreakMinutes,
+      }}>
+        {showSettings ? <Settings /> : <Timer/>}
+      </Settingscontext.Provider>
     
 
 
-  //   </main>
-  // )
+    </main>
+  )
 
-      return(
-        <Todolist />
-      )
+//       return(
+//         <Todolist />
+//       )
 };
 
 export default App;
