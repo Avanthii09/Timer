2fdfb2ed20a4d0ed9894e20e3977e7287422c97c
Avanthii09 <avanthuram@gmail.com>
2023-04-02 10:52:01 +0530

    added files

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,14 @@
-//import "./Todo.css"
-// import "./App.css"
-import "./Slider.css"
-import Timer from "./components/Timer";
-//import Todolist from "./components/Todolist"    //<Todolist /> in return
+import "./Todo.css"
+import Todolist from "./components/Todolist"   
 import 'bootstrap/dist/css/bootstrap.min.css'
- import {useState} from "react";
- import Settings from "./components/Settings";
- import Settingscontext from "./components/Settingscontext";
+
 
 function App() {
 
- const [showSettings, setShowSettings] = useState(false);
- const [workMinutes, setWorkMinutes] = useState(45);
- const [breakMinutes, setBreakMinutes] = useState(15);
 
-
- 
-  return(
-    <main>
-      <Settingscontext.Provider value={{
-        showSettings,
-        setShowSettings,
-        workMinutes,
-        breakMinutes,
-        setWorkMinutes,
-        setBreakMinutes,
-      }}>
-        {showSettings ? <Settings /> : <Timer/>}
-      </Settingscontext.Provider>
-    
-
-
-    </main>
-  )
-
-//       return(
-//         <Todolist />
-//       )
+      return(
+        <Todolist />
+      )
 };
 
 export default App;
-
-
-
-
--- src/components/Settingscontext.jsx
+++
@@ -1,5 +0,0 @@
-import react from 'react';
-
-const Settingscontext = react.createContext({});
-
-export default Settingscontext;
